b659e3524dd6b7c6e5ae83151f8326792ee1ee66
mariobob <35691890+Mariobob@users.noreply.github.com>
2021-07-19 16:55:09 -0400

    format

--- 2-aur.sh
+++ 2-aur.sh
@@ -9,15 +9,12 @@ git clone https://aur.archlinux.org/paru.git ~/.build/paru
 cd ~/.build/paru
 makepkg -si --noconfirm
 
-
-
 PKGS=(
     'alacritty-git'
     'sassc-git'
     'mint-themes-git'
     'plymouth'
 )
-
 
 for PKG in "${PKGS[@]}"; do
     echo "INSTALLING: ${PKG}"
--- 3-de.sh
+++ 3-de.sh
@@ -2,22 +2,18 @@ mkdir ~/.config
 git clone https://github.com/thevinter/styli.sh ~/.config/styli.sh
 sudo chmod +x ~/.config/styli.sh/styli.sh
 
-
-crontab -l > mycron
-echo "@hourly ~/.config/styli.sh/styli.sh" >> mycron
+crontab -l >mycron
+echo "@hourly ~/.config/styli.sh/styli.sh" >>mycron
 crontab mycron
 rm mycron
-
-
 
 PKGS=(
     'cinnamon'
 )
 
-echo "exec cinnamon-session" >> ~/.xinitrc
+echo "exec cinnamon-session" >>~/.xinitrc
 
 for PKG in "${PKGS[@]}"; do
     echo "INSTALLING: ${PKG}"
     sudo pacman -S "$PKG" --noconfirm --needed
 done
-
